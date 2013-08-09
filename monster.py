2d1429599282f92dbeb350983f13fa0351aad9eb
laureli <lk.mallek@gmail.com>
2013-08-08 19:16:37 -0700

    commiting start of user interface

--- monster.py
+++ monster.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect, jsonify
+import os
+from flask import Flask, render_template, send_from_directory, request, redirect, jsonify
 import schema
 from schema import Cookie, User, session
+
+from geventwebsocket.handler import WebSocketHandler
+from gevent.pywsgi import WSGIServer
 import json
 
 #configuration information
@@ -16,10 +20,20 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-# @app.route('/view_cookies', methods=['POST'], ['GET'])
-# def cookie_view():
-# 	return "show me the cookies on your computer, returning user!"
-	# existing user, see cookies there
+@app.route('/socket')
+def socket():
+	if request.environ.get('wsgi.websocket'):
+		ws = request.environ['wsgi.websocket']
+		while True:
+			message = ws.receive()
+			ws.send(message)
+	return
+
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
 
 
 
@@ -33,13 +47,22 @@ def read_cookies():
 		values = c.values()
 		# keys and values are LISTS
 
-		if values[0] == 'www.'+ request.form['requested_domain']+'.com':
+		if values[0] == 'www.'+ request.form['requested_domain']:
 			cookie_object = Cookie()
 			cookie_object.set_cookie_from_browser(c)
 		        session.add(cookie_object)
 			session.commit()
 
-	return jsonify(content)
+	# return jsonify(content)
+	return redirect("/show_cookies.html")
+
+
+
+@app.route('/show_cookies')
+def show_cookies():
+	# return " here is where we designate domain to search, in html"
+	return render_template("show_cookies.html")
+
 
 
 @app.route('/set_browser_cookie', methods=['GET'])
@@ -48,10 +71,10 @@ def set_browser_cookie():
 # REMEMBER: url and domain need to be THE SAME PLACE !!!
 
 	return jsonify({"cookies": [
-		{"url": "http://www.snazzy.com", 
-		"name": "testCookie2", 
-		"value": "sample-value-here2",
-		"domain": "snazzy.com",}
+		{"url": "http://www.snaps.com", 
+		"name": "testCookie3", 
+		"value": "sample-value-here22",
+		"domain": "snaps.com",}
 		]
 	})
 
@@ -59,6 +82,10 @@ def set_browser_cookie():
 def call_cookies():
 	# return " here is where we designate domain to search, in html"
 	return render_template("call_cookies.html")
+
+@app.route('/test')
+def test():
+	return render_template('test.html')
 
 
 @app.route('/') # index!
@@ -70,12 +97,10 @@ def index():
 
 
 
+
+
 # ################## 
 	
-
-@app.route('/login')
-def login():
-	return "hello, login here in html"
 
 @app.route('/welcome') # splash page
 def welcome():
@@ -88,10 +113,6 @@ def sign_up():
 	# new user sign up here
 
 
-
-
-
-
 @app.route('/_add_numbers')
 def add_numbers():
     a = request.args.get('a', 0, type=int)
@@ -100,14 +121,20 @@ def add_numbers():
     return jsonify(result=a + b * c)
 
 
-import os
-from flask import send_from_directory
-
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
+
+# @app.route('/view_cookies', methods=['POST'], ['GET'])
+# def cookie_view():
+# 	return "show me the cookies on your computer, returning user!"
+	# existing user, see cookies there
+
+
 if __name__ == "__main__":
+	# http_server = WSGIServer(('',5000), app, handler_class=WebSocketHandler)
+	# http_server.serve_forever()
 	app.run(debug=True)
